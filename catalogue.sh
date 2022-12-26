d7f8b1aca7334df176130694dfc51099731de6ef
shwetasna <jenashwetasna@gmail.com>
2022-12-26 09:40:49 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -10,3 +10,10 @@ unzip -o /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
+
+sed -i -e 's/MONGO_DNSNAME/mongodb.shwetadevopspractice.online/' systemd.service
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl restart catalogue
+systemctl enable catalogue
